95514dd4a5123f365c5bb19785d0ba96cbf8956b
Divyansh Singh <divyanshsinghh2304@gmail.com>
2026-03-08 21:09:27 +0530

    Add . Method Operation is being performed"

---
+++ Tuple/TupleMethod.py
@@ -0,0 +1,12 @@
+# create tuple with duplicate elements
+tup = (1, 2, 3, 2, 4, 2, 5)
+
+print("Tuple:", tup)
+
+# count occurrences of element
+count_element = tup.count(2)
+print("Occurrences of 2:", count_element)
+
+# find index of first occurrence
+index_element = tup.index(2)
+print("First index of 2:", index_element)
