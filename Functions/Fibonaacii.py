da782d4421b01135f97465d23d1760ccdd482f15
Divyansh Singh <divyanshsinghh2304@gmail.com>
2026-03-09 09:18:10 +0530

    Add . Started doing functions

---
+++ Functions/Fibonaacii.py
@@ -0,0 +1,10 @@
+def fibonacci(n,memo={}):
+    if n<=1:
+        return n
+    if n in memo:
+        return memo[n]
+    memo[n] = fibonacci(n-1,memo) + fibonacci(n-2,memo)
+    return memo[n]
+
+print(fibonacci(10))
+print(fibonacci(100))
